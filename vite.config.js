b210c5b864dbf03a5fde7aef68f928495f03e610
Abrahan Tuñon <abrahan.tunon@utp.ac.pa>
2025-12-08 23:02:46 -0500

    Hola

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  // **ESTO ES LO QUE ARREGLA EL PUNTO DE ENTRADA EN GITHUB PAGES**
 base: 'https://TvAbrahan.github.io/vinted-project/', 
   
   plugins: [vue()],
