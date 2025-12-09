b8cb759ec652a22aeafca461fa8c1dc5845bf959
Abrahan Tuñon <abrahan.tunon@utp.ac.pa>
2025-12-08 22:42:25 -0500

    agregando cambios del vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   // **ESTO ES LO QUE ARREGLA EL PUNTO DE ENTRADA EN GITHUB PAGES**
-// base: '/vinted-project/', 
+base: 'https://TvAbrahan.github.io/vinted-project/', 
   
   plugins: [vue()],
 })
